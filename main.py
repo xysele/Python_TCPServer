f09a67880884984c7317dd0f42ecf17c9bbfb836
xysele <162809645+xysele@users.noreply.github.com>
2024-04-13 20:56:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import socketserver
 import subprocess
 import os
 
-PORT = 8080
+PORT = 3000
 Handler = http.server.SimpleHTTPRequestHandler
-
-frpc_path = "./frpc"
-os.chmod(frpc_path, 0o755)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving HTTP at port", PORT)
     httpd.serve_forever()
-
-frpc_process = subprocess.Popen([frpc_path, "-c", "frpc.toml"])
-
-frpc_process.wait()
